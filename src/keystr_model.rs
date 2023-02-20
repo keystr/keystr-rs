38fddd05eaa177237cb2fe7c3fc045e3822cf3f0
Adam B <13562139+catenocrypt@users.noreply.github.com>
2023-02-20 23:57:20 +0100

    Add status msg on UI, error handling

--- src/keystr_model.rs
+++ src/keystr_model.rs
@@ -3,6 +3,7 @@ use crate::{delegator::Delegator, keystore::Keystore};
 pub(crate) struct KeystrModel {
     pub own_keys: Keystore,
     pub delegator: Delegator,
+    pub status_line: String,
 }
 
 impl KeystrModel {
@@ -10,6 +11,15 @@ impl KeystrModel {
         KeystrModel {
             own_keys: Keystore::new(),
             delegator: Delegator::new(),
+            status_line: String::new(),
         }
+    }
+
+    pub fn set_status(&mut self, s: &str) {
+        self.status_line = s.to_string();
+    }
+
+    pub fn set_error_status(&mut self, s: &str) {
+        self.status_line = format!("Error: {}!", s.to_string());
     }
 }
