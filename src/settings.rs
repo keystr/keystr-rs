cc2f363ddcf1e01cdf14e937384b2fd1d48554b6
Adam B <13562139+catenocrypt@users.noreply.github.com>
2023-03-02 00:26:56 +0100

    Missing files, Persist settings

---
+++ src/settings.rs
@@ -0,0 +1,32 @@
+use crate::error::Error;
+use crate::security_settings::{SecurityLevel, SecuritySettings};
+use crate::storage::Storage;
+use serde::{Deserialize, Serialize};
+use std::fs;
+
+/// Settings
+#[readonly::make]
+#[derive(Default, Serialize, Deserialize)]
+pub struct Settings {
+    #[readonly]
+    pub security: SecuritySettings,
+}
+
+impl Settings {
+    pub fn set_security_level(&mut self, level: SecurityLevel) {
+        self.security.security_level = level;
+        let _res = self.save();
+    }
+
+    pub fn save(&self) -> Result<(), Error> {
+        let str = serde_json::to_string(&self)?;
+        Storage::check_create_folder()?;
+        fs::write(Storage::settings_file(), str)?;
+        Ok(()) // TODO
+    }
+
+    pub fn load() -> Result<Self, Error> {
+        let str = fs::read_to_string(Storage::settings_file())?;
+        Ok(serde_json::from_str::<Self>(&str)?)
+    }
+}
---
+++ src/storage.rs
@@ -0,0 +1,49 @@
+use crate::error::Error;
+use std::fs;
+use std::path::PathBuf;
+
+pub(crate) struct Storage {}
+
+/// Folder used to store data, relative to user data dir (~/.local/share)
+const LOCAL_STORAGE_FOLDER: &str = "keystr";
+/// Public key storage file name, relative to data folder.
+const PUBLIC_KEY_FILENAME: &str = "npub";
+/// Encrypted secret key storage file name, relative to data folder.
+const ENCRYPTED_SECRET_KEY_FILENAME: &str = ".ncrypt";
+/// Public key storage file name, relative to data folder.
+const SETTINGS_FILENAME: &str = "settings.json";
+
+impl Storage {
+    pub fn public_key_file() -> PathBuf {
+        Self::full_file_path(PUBLIC_KEY_FILENAME)
+    }
+
+    pub fn encrypted_secret_key_file() -> PathBuf {
+        Self::full_file_path(ENCRYPTED_SECRET_KEY_FILENAME)
+    }
+
+    pub fn settings_file() -> PathBuf {
+        Self::full_file_path(SETTINGS_FILENAME)
+    }
+
+    pub fn check_create_folder() -> Result<(), Error> {
+        let p = Self::get_storage_folder();
+        if p.is_dir() {
+            return Ok(());
+        }
+        fs::create_dir(p)?;
+        Ok(())
+    }
+
+    fn get_storage_folder() -> PathBuf {
+        let mut p = dirs::data_local_dir().unwrap_or(PathBuf::from("."));
+        p.push(LOCAL_STORAGE_FOLDER);
+        p
+    }
+
+    fn full_file_path(file_name: &str) -> PathBuf {
+        let mut p = Self::get_storage_folder();
+        p.push(file_name);
+        p
+    }
+}
